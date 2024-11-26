cf4aa9423e3292f9544a736ec8bbe53e80afdb8e
Carlos Enrique Cimino <ciminoce@yahoo.com.ar>
2024-11-26 18:04:01 -0300

    Agregar archivos de proyecto.

---
+++ PrefinalProgramacionPunto3D.Windows/frmPuntos.cs
@@ -0,0 +1,10 @@
+namespace PrefinalProgramacionPunto3D.Windows
+{
+    public partial class frmPuntos : Form
+    {
+        public frmPuntos()
+        {
+            InitializeComponent();
+        }
+    }
+}
